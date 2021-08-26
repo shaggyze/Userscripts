65ef672242508c56c9a0565d1cae9401d204dfac
shaggyze <shaggyze@gmail.com>
2021-08-26 04:24:51 -0700

    Update Last Post - MAL.js
    Removed unneeded float: right;

--- Last Post - MAL.js
+++ Last Post - MAL.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Last Post - MAL
 // @namespace   Last_Post
-// @version     0.5
+// @version     0.6
 // @description Add Last Post link to MAL Forum Topics.
 // @author      ShaggyZE & hacker09
 // @include      *://myanimelist.net/forum/?topicid=*
@@ -13,7 +13,7 @@
   'use strict';
   var href = location.href;
   href = href.replace(/\#.*/,'');
-  document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4 > div").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;float: right;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
+  document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4 > div").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
   document.querySelector("#content").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;float: right;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
   document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4").insertAdjacentHTML('beforeEnd', `<a style="cursor: pointer;" onclick='[...document.querySelectorAll("div.forum-topic-message-wrapper")].pop().scrollIntoView()'>Bottom</a>`); //Add the  link before first post of current page.
 })();
