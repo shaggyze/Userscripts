8518043e91518b525f078c7b8092883d6d58f043
shaggyze <shaggyze@gmail.com>
2021-08-26 22:23:13 -0700

    Update Last Post - MAL.js
    Add Last Post link to notifications.

--- Last Post - MAL.js
+++ Last Post - MAL.js
@@ -1,19 +1,30 @@
 // ==UserScript==
 // @name        Last Post - MAL
 // @namespace   Last_Post
-// @version     0.6
+// @version     0.7
 // @description Add Last Post link to MAL Forum Topics.
 // @author      ShaggyZE & hacker09
-// @include      *://myanimelist.net/forum/?topicid=*
+// @include      *://myanimelist.net/*
 // @icon        https://www.google.com/s2/favicons?domain=myanimelist.net
 // @run-at      document-end
 // ==/UserScript==
-
+var TimesExecuted;
 (function() {
   'use strict';
   var href = location.href;
-  href = href.replace(/\#.*/,'');
-  document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4 > div").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
-  document.querySelector("#content").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;float: right;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
-  document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4").insertAdjacentHTML('beforeEnd', `<a style="cursor: pointer;" onclick='[...document.querySelectorAll("div.forum-topic-message-wrapper")].pop().scrollIntoView()'>Bottom</a>`); //Add the  link before first post of current page.
+  if (href.match(/topicid/) !== null) {
+    href = href.replace(/\#.*/,'');
+    document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4 > div").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
+    document.querySelector("#content").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;float: right;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
+    document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4").insertAdjacentHTML('beforeEnd', `<a style="cursor: pointer;" onclick='[...document.querySelectorAll("div.forum-topic-message-wrapper")].pop().scrollIntoView()'>Bottom</a>`); //Add the  link before first post of current page.
+  }
+  document.querySelector("div.header-menu-unit.header-notification").onmouseover = function() {
+  if (TimesExecuted == undefined) {
+    document.querySelectorAll("ol.header-notification-item-list > li > div > div:nth-child(2) > span > a:nth-child(2)").forEach(function(el){
+    if (el.href.match(/topicid/) !== null) {
+      el.insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;" href="` + el.href + `&goto=lastpost">Last Post »»</a>`); //
+    }
+    TimesExecuted += 1;;
+    })
+  }}
 })();
