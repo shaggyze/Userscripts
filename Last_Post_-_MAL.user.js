ab50ec7ef69170fa43542517b830432b21c6b4f9
shaggyze <shaggyze@gmail.com>
2022-07-14 05:17:56 -0700

    Update Last_Post_-_MAL.user.js
    MAL added recent board topics and watched topics to the bottom of threads now so Last Post link needed to be fixed to be above them.

--- Last_Post_-_MAL.user.js
+++ Last_Post_-_MAL.user.js
@@ -4,10 +4,10 @@
 // @updateURL   https://openuserjs.org/meta/shaggyze/Last_Post_-_MAL.meta.js
 // @downloadURL https://openuserjs.org/install/shaggyze/Last_Post_-_MAL.user.js
 // @copyright   2022, shaggyze (https://openuserjs.org/users/shaggyze)
-// @version     1.2
+// @version     1.3
 // @description Add Last Post link to MAL Forum Topics.
 // @author      ShaggyZE & hacker09
-// @include      *://myanimelist.net/*
+// @match      *://myanimelist.net/*
 // @icon        https://dl.dropboxusercontent.com/s/yics96pcxixujd1/MAL.png
 // @run-at      document-end
 // @license     MIT; https://opensource.org/licenses/MIT
@@ -19,8 +19,8 @@ var TimesExecuted;
   if (href.match(/topicid/) !== null) {
     href = href.replace(/\#.*/,'');
     document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4 > div").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
-    document.querySelector("#content").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;float: right;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
-    document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4").insertAdjacentHTML('beforeEnd', `<a style="cursor: pointer;" onclick='[...document.querySelectorAll("div.forum-topic-message-wrapper")].pop().scrollIntoView()'>Bottom</a>`); //Add the  link before first post of current page.
+    document.querySelector("#quickReply").insertAdjacentHTML('beforeBegin', `<a style="cursor: pointer;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link after last post of current page.
+    document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4").insertAdjacentHTML('beforeEnd', `<a style="cursor: pointer;" onclick='[...document.querySelectorAll("div.forum-topic-message-wrapper")].pop().scrollIntoView()'>Bottom</a>`); //Add the Bottom link before first post of current page.
   }
   document.querySelector("div.header-menu-unit.header-notification").onmouseover = function() {
   if (TimesExecuted == undefined) {
