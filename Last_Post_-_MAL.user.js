615b7f89bf6d1702331ca5879ee128d096bc011e
shaggyze <shaggyze@gmail.com>
2022-07-14 09:13:28 -0700

    Update Last_Post_-_MAL.user.js
    Add Last Post link back to bottom right

--- Last_Post_-_MAL.user.js
+++ Last_Post_-_MAL.user.js
@@ -4,8 +4,8 @@
 // @updateURL   https://openuserjs.org/meta/shaggyze/Last_Post_-_MAL.meta.js
 // @downloadURL https://openuserjs.org/install/shaggyze/Last_Post_-_MAL.user.js
 // @copyright   2022, shaggyze (https://openuserjs.org/users/shaggyze)
-// @version     1.3
-// @description Add Last Post link to MAL Forum Topics.
+// @version     1.4
+// @description Add Last Post link to MAL Forum Topics and Notifications.
 // @author      ShaggyZE & hacker09
 // @match      *://myanimelist.net/*
 // @icon        https://dl.dropboxusercontent.com/s/yics96pcxixujd1/MAL.png
@@ -18,9 +18,10 @@ var TimesExecuted;
   var href = location.href;
   if (href.match(/topicid/) !== null) {
     href = href.replace(/\#.*/,'');
+	document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4").insertAdjacentHTML('beforeEnd', `<a style="cursor: pointer;" onclick='[...document.querySelectorAll("div.forum-topic-message-wrapper")].pop().scrollIntoView()'>Bottom</a>`); //Add the Bottom link before first post of current page.
     document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4 > div").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link before first post of current page.
     document.querySelector("#quickReply").insertAdjacentHTML('beforeBegin', `<br><a style="cursor: pointer;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link after last post of current page.
-    document.querySelector("div.mt4.mb4.pl0.pb0.pt4.pb4").insertAdjacentHTML('beforeEnd', `<a style="cursor: pointer;" onclick='[...document.querySelectorAll("div.forum-topic-message-wrapper")].pop().scrollIntoView()'>Bottom</a>`); //Add the Bottom link before first post of current page.
+    document.querySelector("#content").insertAdjacentHTML('beforeEnd', `&nbsp;<a style="cursor: pointer;float: right;" href="` + href + `&goto=lastpost">Last Post »»</a>`); //Add the Last Post link after topics of current page.
   }
   document.querySelector("div.header-menu-unit.header-notification").onmouseover = function() {
   if (TimesExecuted == undefined) {
